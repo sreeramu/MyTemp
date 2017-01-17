f2a0d217a8006d6f4a4ef664de9f99d7056d6cbf
Sreenivasan K <sreeramu@gmail.com>
2017-01-17 23:23:36 +0530

    Initial commit

---
+++ app/src/main/java/com/sreeramu/spinner/MainActivity.java
@@ -0,0 +1,40 @@
+package com.sreeramu.spinner;
+
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
+import com.sreeramu.spinner.materialspinner.CustomSpinner;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String[] ANDROID_VERSIONS = {
+            "Cupcake", "Donut", "Eclair", "Froyo", "Gingerbread", "Honeycomb", "Ice Cream Sandwich", "Jelly Bean", "KitKat",
+            "Lollipop", "Marshmallow"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        CustomSpinner spinner = (CustomSpinner) findViewById(R.id.spinner);
+        spinner.setItems(ANDROID_VERSIONS);
+        spinner.setOnItemSelectedListener(new CustomSpinner.OnItemSelectedListener<String>() {
+
+            @Override
+            public void onItemSelected(CustomSpinner view, int position, long id, String item) {
+                Snackbar.make(view, "Clicked " + item, Snackbar.LENGTH_LONG).show();
+            }
+        });
+        spinner.setOnNothingSelectedListener(new CustomSpinner.OnNothingSelectedListener() {
+
+            @Override
+            public void onNothingSelected(CustomSpinner spinner) {
+                Snackbar.make(spinner, "Nothing selected", Snackbar.LENGTH_LONG).show();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/sreeramu/spinner/materialspinner/CustomSpinnerAdapter.java
@@ -0,0 +1,36 @@
+package com.sreeramu.spinner.materialspinner;
+
+import android.content.Context;
+
+import java.util.List;
+
+public class CustomSpinnerAdapter<T> extends CustomSpinnerBaseAdapter {
+
+  private final List<T> items;
+
+  public CustomSpinnerAdapter(Context context, List<T> items) {
+    super(context);
+    this.items = items;
+  }
+
+  @Override public int getCount() {
+    return items.size() - 1;
+  }
+
+  @Override public T getItem(int position) {
+    if (position >= getSelectedIndex()) {
+      return items.get(position + 1);
+    } else {
+      return items.get(position);
+    }
+  }
+
+  @Override public T get(int position) {
+    return items.get(position);
+  }
+
+  @Override public List<T> getItems() {
+    return items;
+  }
+
+}
